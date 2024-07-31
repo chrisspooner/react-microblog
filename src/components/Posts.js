d56b3c2bef62103c6b6968157ae08d6563b860aa
Chris Spooner <chris.spooner12@gmail.com>
2024-07-31 15:48:53 -0400

    Break out API calls to use a custom hook. Add Pagination to pages of posts. Add Explore Page and User Page

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,34 +1,60 @@
 import { useState, useEffect } from "react";
 import { Spinner } from "react-bootstrap";
+import { useApi } from "../contexts/ApiProvider";
 import Post from './Post';
+import More from "./More";
 
-const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
+export default function Posts({ content }) {
+    const [ posts, setPosts ] = useState();
+    const [ pagination, setPagination ] = useState();
+    const api = useApi();
 
-export default function Posts() {
-const [ posts, setPosts ] = useState()
+    let url;
+    switch (content) {
+        case 'feed':
+        case undefined:
+            url = '/feed';
+            break;
+        case 'explore':
+            url = '/posts';
+            break;
+        default:
+            url = `/users/${content}/posts`;
+            break;
+    }
 
 
     useEffect(() => {
         (async () => {
-            const response = await fetch(BASE_API_URL + '/api/feed');
-            if (response.ok) {
-                const results = await response.json();
-                setPosts(results.data);
-            }
-            else {
-                setPosts(null);
-            }
+          const response = await api.get(url);
+          if (response.ok) {
+            setPosts(response.body.data);
+            setPagination(response.body.pagination);
+          }
+          else {
+            setPosts(null);
+          }
         })();
-    }, []);
-
-    return (
+      }, [api, url]);
+    
+      const loadNextPage = async () => {
+        const response = await api.get(url, {
+          after: posts[posts.length - 1].timestamp
+        });
+        if (response.ok) {
+          setPosts([...posts, ...response.body.data]);
+          setPagination(response.body.pagination);
+        }
+      };
+    
+      return (
         <>
           {posts === undefined ?
             <Spinner animation="border" />
           :
             <>
               {posts === null ?
-                 <p>Could not retrieve blog posts.</p>
+                <p>Could not retrieve blog posts.</p>
               :
                 <>
                   {posts.length === 0 ?
@@ -40,6 +66,7 @@ const [ posts, setPosts ] = useState()
               }
             </>
           }
+          <More pagination={pagination} loadNextPage={loadNextPage} />
         </>
       );
-}
+    }
