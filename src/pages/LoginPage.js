6d0497da51e9fb1c660f6cffc278ddcd249eb959
Chris Spooner <chris.spooner12@gmail.com>
2024-07-31 19:14:09 -0400

    Added login and registration workflow. Created flashMessage context

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,55 @@
+import { useState, useRef, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import Body from "../components/Body";
+import InputField from "../components/InputField";
+import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
+    const [ formErrors, setFormErrors ] = useState({});
+    const usernameField = useRef();
+    const passwordField = useRef();
+
+    useEffect(() => {
+        usernameField.current.focus();
+    }, [])
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+        const username = usernameField.current.value;
+        const password = usernameField.current.value;
+
+        
+        const errors = {};
+        if (!username) {
+            errors.username = 'Username must not be empty.';
+        }
+        if (!password) {
+            errors.password = 'Password must not be empty.';
+        }
+
+        setFormErrors(errors);
+        if (Object.keys(errors).length > 0) {
+            return;
+        }
+
+        // To-Do Log the user in
+    };
+
     return (
         <Body>
-            <h1>Login form</h1>
-            <p>To-Do</p>
+            <h1>Login</h1>
+            <Form onSubmit={onSubmit}>
+                <InputField
+                    name="username" label="Username or email address"
+                    error={formErrors.username} fieldRef={usernameField} />
+                <InputField
+                    name="password" label="Password" type="password"
+                    error={formErrors.password} fieldRef={passwordField} />
+                <Button variant="primary" type="submit">Login</Button>
+            </Form>
+            <hr />
+            <p>Don&apos;t have an account? <Link to="/register">Register here</Link>!</p>
         </Body>
     );
 }
