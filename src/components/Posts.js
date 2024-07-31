efd8194087a3ff7fdc5ee781fdf94544d4eb987e
Chris Spooner <chris.spooner12@gmail.com>
2024-07-30 21:49:49 -0400

    Pulled posts and Users from API. Formatted timestamp from posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,38 +1,45 @@
+import { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
+import Post from './Post';
+
+const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
+
 export default function Posts() {
-    const posts = [
-        {
-          id: 1,
-          text: 'Hello, world!',
-          timestamp: 'a minute ago',
-          author: {
-            username: 'susan',
-          },
-        },
-        {
-          id: 2,
-          text: 'Second Post',
-          timestamp: 'an hour ago',
-          author: {
-            username: 'john',
-          },
-        },
-      ];
+const [ posts, setPosts ] = useState()
+
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(BASE_API_URL + '/api/feed');
+            if (response.ok) {
+                const results = await response.json();
+                setPosts(results.data);
+            }
+            else {
+                setPosts(null);
+            }
+        })();
+    }, []);
 
     return (
         <>
-            {posts.length === 0 ?
-                <p>There is nothing to see here</p>
-            :
-                posts.map(post => {
-                    return (
-                    <p key={post.id}>
-                        <b>{post.author.username}</b> &mdash; {post.timestamp}
-                        <br />
-                        {post.text}
-                    </p>
-                    );
-                })
-            }
+          {posts === undefined ?
+            <Spinner animation="border" />
+          :
+            <>
+              {posts === null ?
+                 <p>Could not retrieve blog posts.</p>
+              :
+                <>
+                  {posts.length === 0 ?
+                    <p>There are no blog posts.</p>
+                  :
+                    posts.map(post => <Post key={post.id} post={post} />)
+                  }
+                </>
+              }
+            </>
+          }
         </>
-    );
+      );
 }
