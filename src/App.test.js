62fbadebf9f3ec7aaafbe502e4c76473e1341de6
Chris Spooner <chris.spooner12@gmail.com>
2024-08-04 17:41:24 -0400

    Add Unit Tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const element = screen.getByText(/Microblog/);
+  
+  expect(element).toBeInTheDocument();
+  expect(element).toHaveClass('navbar-brand');
 });
